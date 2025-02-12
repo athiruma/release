2ccf0ac778167d9e06969bb5b600fde0ff499209
Thirumalesh Aaraveti <athiruma@redhat.com>
2025-02-12 18:33:41 +0530

    print files

--- ci-operator/step-registry/openshift-qe/cloud-governance/perf-dept/openshift-qe-cloud-governance-perf-dept-commands.sh
+++ ci-operator/step-registry/openshift-qe/cloud-governance/perf-dept/openshift-qe-cloud-governance-perf-dept-commands.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cat "/secret/perf-dept-creds" > ./perf-creds.sh
-# chmod 777 ./perf-creds.sh
-ls -la
-# source ./perf-creds.sh
+cat "/secret/perf-dept-creds"  > /tmp/creds.sh
+chmod 755 /tmp/creds.sh
+source /tmp/creds.sh
+export es_host="$(cat /secret/es_host)"
+export es_port="$(cat /secret/es_port)"
 
 python3 /usr/local/cloud_governance/main.py
 
